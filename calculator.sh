7a63440f9caccf191523ffcb43c23dc39da54c25
nikiani <nikianiautomation@gmail.com>
2025-09-28 15:27:29 +0530

    all in one

--- calculator.sh
+++ calculator.sh
@@ -3,6 +3,10 @@ echo "enter input:"
 read a 
 if [ "$a" = "add" ]; then
   sh addition.sh
+elif [ "$a" = "sub" ]; then
+  sh subtraction.sh
+elif [ "$a" = "mul" ]; then
+  sh multi.sh
 else
    echo "not valid input."
  fi
